c057321a2fbce9af8f534da411e10753dac23e16
calguts <cal.gutierrez@umontreal.ca>
2025-07-03 17:14:52 -0400

    idk what im doing

---
+++ src/main/java/com/example/prog2tp1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.prog2tp1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Fourre ta mere");
+    }
+}
