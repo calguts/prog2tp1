2f2c2f3a89fe7030e4f2e4ff29e46c4a821e18b6
calguts <cal.gutierrez@umontreal.ca>
2025-07-08 16:14:07 -0400

    GUI pour les classes en cours

--- src/main/java/com/example/prog2tp1/CoursClasse.java
+++ src/main/java/com/example/prog2tp1/CoursClasse.java
@@ -4,9 +4,25 @@ import java.util.LinkedList;
 
 public class CoursClasse {
 
-    int nbCredits;
     String nomCours;
+    int nbCredits;
     LinkedList<SectionClass> horaireCours;
 
+    public CoursClasse() {}
 
+    public String getNomCours() {
+        return nomCours;
+    }
+
+    public void setNomCours(String nomCours) {
+        this.nomCours = nomCours;
+    }
+
+    public int getNbCredits() {
+        return nbCredits;
+    }
+
+    public void setNbCredits(int nbCredits) {
+        this.nbCredits = nbCredits;
+    }
 }
--- src/main/java/com/example/prog2tp1/CoursController.java
+++ src/main/java/com/example/prog2tp1/CoursController.java
@@ -1,8 +1,14 @@
 package com.example.prog2tp1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class CoursController {
 
+    public void onFermerButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteSelectedClick(ActionEvent actionEvent) {
+    }
 }
