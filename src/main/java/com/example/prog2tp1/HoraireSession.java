c3199e7fc60b3a29c46c341e15103f2acffcdcb6
Thomas-Ring <ringthomas4321@gmail.com>
2025-07-10 22:22:52 -0400

    Ajouté une partie de l'algo

--- src/main/java/com/example/prog2tp1/HoraireSession.java
+++ src/main/java/com/example/prog2tp1/HoraireSession.java
@@ -1,6 +1,7 @@
 package com.example.prog2tp1;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class HoraireSession {
 
@@ -13,13 +14,30 @@ public class HoraireSession {
 
 
     private ArrayList<CoursClasse> gererMonHoraire(int numCredits) {
-        boolean horaireFini = false;
-
-
-        if (!horaireFini) {
-
-            return null;
+        for(int i = 0; i < 10; i++){
+            for(int j = i + 1; j < 10; j++){
+                for(int k =  j + 1; k < 10; k++){
+                    if (aucunConflitHoraire(i, j, k)){
+                        if (bonNombreCredits(i, j, k)){
+                            monHoraire.add(CoursListe.getCours(i));
+                            monHoraire.add(CoursListe.getCours(j));
+                            monHoraire.add(CoursListe.getCours(k));
+                            return monHoraire;
+                        }
+                    }
+                }
+            }
         }
         return monHoraire;
+    }
+
+    private boolean aucunConflitHoraire(int indexCours1, int indexCours2, int indexCours3){
+        if return true;
+    }
+
+    private boolean bonNombreCredits(int indexCours1, int indexCours2, int indexCours3, int numCredits){
+        return CoursListe.getCours(indexCours1).getNbCredits()
+        + CoursListe.getCours(indexCours2).getNbCredits()
+        + CoursListe.getCours(indexCours3).getNbCredits() == numCredits;
     }
 }
